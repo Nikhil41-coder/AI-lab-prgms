31114b2a5fa93503ad262335727e26d7aea9db54
Nikhil H N <2024ci_nikhilhn_b@nie.ac.in>
2026-04-09 12:24:24 +0530

    Add files via upload

---
+++ dfs.py
@@ -0,0 +1,23 @@
+# Define a simple tree using adjacency list representation
+tree = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F', 'G'],
+    'D': [],
+    'E': [],
+    'F': [],
+    'G': []
+}
+
+def dfs(node, visited, tree):
+    if node not in visited:
+        visited.append(node)
+        # Recursively visit each neighbor
+        for neighbor in tree[node]:
+            dfs(neighbor, visited, tree)
+
+    return visited
+
+# Run DFS starting from root 'A'
+visited_order = dfs('A', [], tree)
+print("Order of nodes visited:", visited_order)
